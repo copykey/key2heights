ec3c92feaa9c73f4ce6857de2524d8dec2b9eb5b
Russell Coleman <russellsayshi@gmail.com>
2018-10-20 23:16:59 -0500

    added live

---
+++ key2heights/live.py
@@ -0,0 +1,19 @@
+import cv2
+import find_rectangle
+
+video = cv2.VideoCapture(0)
+
+while(True):
+	ret, frame = cap.read()
+	if ret == False:
+		break
+	success, key, drawn = find_rectangle.process(frame)
+	if success:
+		cv2.imshow("key", key)
+	cv2.imshow("drawn", drawn)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+cap.release()
+cv2.destroyAllWindows()
+
